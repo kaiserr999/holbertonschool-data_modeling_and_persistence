6ec6660660f93d9c5eb4b58965f5dd7e5cf486f5
Kaiserr999 <I.camelin74@gmail.com>
2026-06-03 13:16:59 +0200

    Update task 0: Make all columns NOT NULL

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    genre TEXT,
+    genre TEXT NOT NULL,
     price REAL NOT NULL,
     stock INTEGER NOT NULL,
-    published_year INTEGER
+    published_year INTEGER NOT NULL
 );
